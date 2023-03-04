338c742c777299665ccff603dfb6b7fd9f380034
mharges <michael.harges@gmail.com>
2023-03-03 17:59:15 -0800

    ApproximatelyEqual benchmarks

--- DbC.Net.Tests.Benchmarks/GlobalUsings.cs
+++ DbC.Net.Tests.Benchmarks/GlobalUsings.cs
@@ -2,5 +2,6 @@
 global using BenchmarkDotNet.Running;
 
 global using DbC.Net.ExceptionFactories;
+global using DbC.Net.FloatingPoint;
 global using DbC.Net.TestAndExampleResources;
 global using DbC.Net.Tests.Benchmarks;
