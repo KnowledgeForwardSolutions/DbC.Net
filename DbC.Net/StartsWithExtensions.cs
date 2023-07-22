ba322debc304cdb3e926d57d0a0e2b0f4019c703
mharges <michael.harges@gmail.com>
2023-07-21 18:34:22 -0700

    EnsuresStartsWith and associated unit tests

--- DbC.Net/StartsWithExtensions.cs
+++ DbC.Net/StartsWithExtensions.cs
@@ -8,6 +8,69 @@ public static class StartsWithExtensions
    private const String _requirementName = RequirementNames.StartsWith;
 
    /// <summary>
+   ///   String StartsWith postcondition. Confirm that the <see cref="String"/>
+   ///   <paramref name="value"/> starts with the  <paramref name="target"/> 
+   ///   substring and throw an exception if it does not.
+   /// </summary>
+   /// <param name="value">
+   ///   The value to check.
+   /// </param>
+   /// <param name="target">
+   ///   The target substring that <paramref name="value"/> should start with.
+   /// </param>
+   /// <param name="comparisonType">
+   ///   Optional.  <see cref="StringComparison"/> enumeration value that 
+   ///   specifies how the <paramref name="value"/> and <paramref name="target"/> 
+   ///   strings are compared. Defaults to <see cref="StringComparison.Ordinal"/>.
+   /// </param>
+   /// <param name="messageTemplate">
+   ///   Optional. The message template to use if an exception is thrown.
+   ///   Defaults to "{RequirementType} {RequirementName} failed: {Value} must start with the substring "{Target}"".
+   /// </param>
+   /// <param name="exceptionFactory">
+   ///   Optional. The <see cref="IExceptionFactory"/> used to create the
+   ///   exception that is thrown if the <paramref name="value"/> is 
+   ///   <see langword="null"/>. Defaults to 
+   ///   <see cref="StandardExceptionFactories.PostconditionFailedExceptionFactory"/>.
+   /// </param>
+   /// <param name="valueExpression">
+   ///   Optional. Defaults to the caller expression for
+   ///   <paramref name="value"/>. 
+   /// </param>
+   /// <param name="targetExpression">
+   ///   Optional. Defaults to the caller expression for
+   ///   <paramref name="target"/>. 
+   /// </param>
+   /// <returns>
+   ///   The tested <paramref name="value"/> is returned unaltered to support 
+   ///   chaining requirements.
+   /// </returns>
+   /// <exception cref="ArgumentNullException">
+   ///   <paramref name="target"/> is <see langword="null"/>.
+   /// </exception>
+   public static String EnsuresStartsWith(
+      this String value,
+      String target,
+      StringComparison comparisonType = StringComparison.Ordinal,
+      String? messageTemplate = null,
+      IExceptionFactory? exceptionFactory = null,
+      [CallerArgumentExpression("value")] String valueExpression = null!,
+      [CallerArgumentExpression("target")] String targetExpression = null!)
+   {
+      CheckStartsWith(
+         value,
+         target ?? throw new ArgumentNullException(nameof(target), Messages.TargetSubstringIsNull),
+         comparisonType,
+         RequirementType.Postcondition,
+         messageTemplate,
+         exceptionFactory,
+         valueExpression,
+         targetExpression);
+
+      return value;
+   }
+
+   /// <summary>
    ///   String StartsWith precondition. Confirm that the <see cref="String"/>
    ///   <paramref name="value"/> starts with the  <paramref name="target"/> 
    ///   substring and throw an exception if it does not.
